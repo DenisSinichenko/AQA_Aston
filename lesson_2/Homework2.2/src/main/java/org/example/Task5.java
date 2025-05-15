1c75533d581151754e068ec7a263d2f39115cb2c
Sinichenko Denis <dzyanis92@gmail.com>
2025-05-15 12:09:56 +0300

    Добавлена ветка lesson_2 по ДЗ к лекции 2.2

---
+++ lesson_2/Homework2.2/src/main/java/org/example/Task5.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Task5 {
+    public boolean compareSum(int a, int b) {
+        int sum = a + b;
+        if ((sum >= 10) && (sum <= 20)) {
+            return true;
+        } else
+            return false;
+    }
+}
---
+++ lesson_2/Homework2.2/src/main/java/org/example/Task7.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Task7 {
+    public boolean positivOrNegativNumber(int a) {
+        if (a >= 0) {
+            return true;
+        } else
+            return false;
+    }
+}
