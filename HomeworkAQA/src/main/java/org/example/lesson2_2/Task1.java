a1adeec6c4ed93641167365313314125f6a0b3c6
Sinichenko Denis <dzyanis92@gmail.com>
2025-05-27 21:12:43 +0300

    Удалены lesson2_2 и lesson2_3 из ветки lesson2_4

--- HomeworkAQA/src/main/java/org/example/lesson2_2/Task1.java
+++
@@ -1,9 +0,0 @@
-package org.example.lesson2_2;
-
-public class Task1 {
-    public void printThreeWords(){
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-}
