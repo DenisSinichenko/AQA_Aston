a50f8ba48994d2441d4adc2639b6623648a45a61
Sinichenko Denis <dzyanis92@gmail.com>
2025-05-30 11:19:21 +0300

    Восстановлена папка lesson2_2

---
+++ HomeworkAQA/src/main/java/org/example/lesson2_2/Task1.java
@@ -0,0 +1,9 @@
+package org.example.lesson2_2;
+
+public class Task1 {
+    public void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
---
+++ HomeworkAQA/src/main/java/org/example/lesson2_2/Task8.java
@@ -0,0 +1,9 @@
+package org.example.lesson2_2;
+
+public class Task8 {
+    public void printStringCount(String s, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(s);
+        }
+    }
+}
