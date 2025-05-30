2224de323a15df8938552fdfd7643065f37b2ec7
Sinichenko Denis <dzyanis92@gmail.com>
2025-05-30 11:49:51 +0300

    Удалил лишние пробелы

--- HomeworkAQA/src/main/java/org/example/lesson2_4/Task2/Main.java
+++ HomeworkAQA/src/main/java/org/example/lesson2_4/Task2/Main.java
@@ -3,7 +3,7 @@ package org.example.lesson2_4.Task2;
 public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle(5, "Красный", "Черный");
-        Rectangle rectangle = new Rectangle(4, 6, "Синий", "Зеленый");
+        Rectangle rectangle = new Rectangle(4, 5, "Синий", "Зеленый");
         Triangle triangle = new Triangle(3.5, 4.9, 2, "Желтый", "Фиолетовый");
         circle.printInfo();
         System.out.println("-----------------------");
