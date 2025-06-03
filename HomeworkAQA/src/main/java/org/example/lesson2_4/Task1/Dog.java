8beb25f30ed0714bcf206c4099d1d548ce0600ed
Sinichenko Denis <dzyanis92@gmail.com>
2025-06-03 10:18:51 +0300

    Редактировано ДЗ к лекции 2_5

--- HomeworkAQA/src/main/java/org/example/lesson2_4/Task1/Dog.java
+++
@@ -1,7 +0,0 @@
-package org.example.lesson2_4.Task1;
-
-public class Dog extends Animal {
-    public Dog(String name) {
-        super(name, 500, 10);
-    }
-}
--- HomeworkAQA/src/main/java/org/example/lesson2_4/Task2/FigureArea.java
+++
@@ -1,6 +0,0 @@
-package org.example.lesson2_4.Task2;
-
-public interface FigureArea {
-    double getPerimeter();
-    double getArea();
-}
