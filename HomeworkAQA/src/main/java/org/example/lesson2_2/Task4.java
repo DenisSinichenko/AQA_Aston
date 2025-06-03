d881879194ea428a2bc080c5f6a28538cbe6db8e
Sinichenko Denis <dzyanis92@gmail.com>
2025-06-03 11:25:50 +0300

    Восстановлены lesson2_2 и lesson2_3

---
+++ HomeworkAQA/src/main/java/org/example/lesson2_2/Task4.java
@@ -0,0 +1,12 @@
+package org.example.lesson2_2;
+
+public class Task4 {
+    public void compareNumbers(){
+        int a = -5, b = 28;
+        if(a >= b){
+            System.out.println("a >= b");
+        }else {
+            System.out.println("a < b");
+        }
+    }
+}
