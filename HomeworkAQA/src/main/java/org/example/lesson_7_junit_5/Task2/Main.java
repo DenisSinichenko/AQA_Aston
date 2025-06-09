9f1c6c3b365cf07d010fcb4efbd3b4e17c132da6
Sinichenko Denis <dzyanis92@gmail.com>
2025-06-09 12:42:50 +0300

    ДЗ 2.7 TestNG

--- HomeworkAQA/src/main/java/org/example/lesson_7_junit_5/Task2/Main.java
+++
@@ -1,7 +0,0 @@
-package org.example.lesson_7_junit_5.Task2;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Площадь треугольника = " + TriangleArea.calculateTriangleArea(5,4));
-    }
-}
--- HomeworkAQA/src/main/java/org/example/lesson_7_junit_5/Task2/TriangleArea.java
+++
@@ -1,7 +0,0 @@
-package org.example.lesson_7_junit_5.Task2;
-
-public class TriangleArea {
-    public static double calculateTriangleArea(int a, int h) {
-        return 0.5 * a * h;
-    }
-}
