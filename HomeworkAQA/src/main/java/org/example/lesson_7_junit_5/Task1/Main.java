f7e4dc79d658d9c51799f34d2f9272cae5195fc1
Sinichenko Denis <dzyanis92@gmail.com>
2025-06-09 13:24:52 +0300

    Удалено лишнее

--- HomeworkAQA/src/main/java/org/example/lesson_7_junit_5/Task1/Main.java
+++
@@ -1,7 +0,0 @@
-package org.example.lesson_7_junit_5.Task1;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Факториал числа = " + Factorial.calculateFactorial(4));
-    }
-}
--- HomeworkAQA/src/main/java/org/example/lesson_7_junit_5/Task4/Main.java
+++
@@ -1,7 +0,0 @@
-package org.example.lesson_7_junit_5.Task4;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Результат сравнения: " + CompareNumbers.compareNumbers(5,1));
-    }
-}
