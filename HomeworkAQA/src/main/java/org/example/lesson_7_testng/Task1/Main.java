d636d31ec463bb9bf7f9c1871f62f88e9d73bdca
Sinichenko Denis <dzyanis92@gmail.com>
2025-06-12 16:25:49 +0300

    Удалены папки lesson_7_testng из ветки lesson2_8

--- HomeworkAQA/src/main/java/org/example/lesson_7_testng/Task1/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example.lesson_7_testng.Task1;
-
-public class Main {
-    public static void main(String[] args) {
-        Factorial factorial = new Factorial();
-        System.out.println("Факториал числа = " + factorial.calculateFactorial(4));
-    }
-}
